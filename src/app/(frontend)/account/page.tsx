10c5db98070e1acecf63b7e3aae748cd45ab6ed7
jamesmaccoy <thankyou.digital@gmail.com>
2026-01-10 10:20:03 +0200

    Implement dynamic rendering for Account page to support cookie-based authentication

--- src/app/(frontend)/account/page.tsx
+++ src/app/(frontend)/account/page.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { getMeUser } from '@/utilities/getMeUser'
 import AccountClient from './page.client'
 
+// Force dynamic rendering since this page uses cookies for authentication
+export const dynamic = 'force-dynamic'
+
 export default async function Account() {
   let user = null
 
